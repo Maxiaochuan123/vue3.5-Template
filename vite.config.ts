9f4b9b4876be7e6a7e8ba67d8af0ff56c68e8c5c
mxc <724503670@qq.com>
2025-01-10 21:04:42 +0800

    feat: 打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
-// https://vite.dev/config/
+// https://vitejs.cn/vite5-cn/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -26,7 +26,33 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    open: true, // 自动打开浏览器
+    open: true,
     cors: true,
   },
+  build: {
+    target: 'modules',
+    minify: 'esbuild',
+    cssMinify: 'esbuild',
+    chunkSizeWarningLimit: 2000,
+    reportCompressedSize: false, // 禁用 gzip 压缩大小报告，提高大型项目的构建性能
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vue-vendor': ['vue', 'vue-router', 'pinia'],
+          'ui-vendor': ['naive-ui', '@vicons/antd', '@vicons/carbon', '@vicons/ionicons5'],
+          'utils-vendor': ['axios', 'echarts', 'crypto-js']
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'pinia', 'axios', 'naive-ui']
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+    legalComments: 'none'
+  }
 })
